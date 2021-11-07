982f57ceadb67dd24668d8ca87dfd4d858d1f33f
Eric Bell <ewbell94@gmail.com>
2021-11-07 16:04:11 -0500

    Changed movement controls to free up buttons

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -7,38 +7,34 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class MovementController : MonoBehaviour
 {
     public XRController teleportRay;
-    public InputHelpers.Button teleportActivationButton;
-    public float activationThreshold = 0.1f;
+    public float activationThreshold = 0.25f;
     public float scrollSpeed=0.5f;
-    private InputDevice targetDevice;
+    private InputDevice rightController;
+    private InputDevice leftController;
     // Update is called once per frame
     void Start() {
         List<InputDevice> devices = new List<InputDevice>();
         InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
         InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
         if (devices.Count > 0){
-            targetDevice=devices[0];
-        } 
+            rightController=devices[0];
+        }
+
+        devices=new List<InputDevice>(); 
+        InputDeviceCharacteristics leftControllerCharacteristics = InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(leftControllerCharacteristics, devices);
+        if (devices.Count > 0){
+            leftController=devices[0];
+        }
     }
     void Update()
     {
-        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool down);
-        targetDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out bool up);
-        if (down){
-            transform.position=new Vector3(transform.position.x,transform.position.y-scrollSpeed,transform.position.z);
+        rightController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 rightStickPos);
+        if (rightStickPos.y > activationThreshold || rightStickPos.y < -activationThreshold){
+            transform.position=new Vector3(transform.position.x,transform.position.y+scrollSpeed*rightStickPos.y,transform.position.z);
         }
 
-        if (up){
-            transform.position=new Vector3(transform.position.x,transform.position.y+scrollSpeed,transform.position.z);
-        }
-
-        if (teleportRay){
-            teleportRay.gameObject.SetActive(CheckIfActivated(teleportRay));
-        }
-    }
-    
-    public bool CheckIfActivated(XRController controller){
-        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThreshold);
-        return isActivated;
+        leftController.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 leftStickPos);
+        teleportRay.gameObject.SetActive(leftStickPos.y > activationThreshold);
     }
 }
